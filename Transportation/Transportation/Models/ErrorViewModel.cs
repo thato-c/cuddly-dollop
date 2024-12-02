d497c98b7779a978c5de1f5932ada93da13e14c2
Thato <t.chitja369@gmail.com>
2024-12-02 11:46:52 +0200

    Create Application

---
+++ Transportation/Transportation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Transportation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
